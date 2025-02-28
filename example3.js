96563baf6127c23e62dee6bf9213fe5f95e48e7b
Neeraj R <125877453+Neeraj5480@users.noreply.github.com>
2025-02-28 16:22:16 +0530

    Add files via upload

---
+++ example3.js
@@ -0,0 +1,27 @@
+
+async function message(num){
+    if(num>0){
+        return('valid');
+    
+    }else{
+        return('invalid')
+    }
+
+}
+message(1).then(
+    (val)=>{
+        console.log(val);
+    },
+    (err)=>{
+        console.log(err);
+    }
+)
+function output(num){
+    console.log('hello neeraj'+num);
+}
+let count=0;
+for(let i=0;i<5;i++){
+    count++;
+    output(count);
+
+}
