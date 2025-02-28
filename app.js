a37fb9ab9ad5f7658fb0ce8d2884d9f94944d2c8
Neeraj R <neerajuchil@gmail.com>
2025-02-28 12:51:50 +0530

    firstcommit

---
+++ app.js
@@ -0,0 +1,17 @@
+const promise1 = new Promise((res,rej)=>{
+    const name='neeraj';
+    if(name=='neeraj'){
+        res('valid');
+    }else{
+        rej('invalid');
+    }
+})
+
+promise1.then(
+    (valid)=>{
+        console.log(`its ${valid}, name is neeraj`);
+    },
+    (error)=>{
+        console.log(`its ${error}, name is not neeraj`);
+    }
+)
